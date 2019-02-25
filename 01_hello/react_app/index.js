a8eb3e4480f5e10817724c48d19de20da481df30
Milo Chen <milo_chen@mail2000.com.tw>
2019-02-25 14:48:57 +0000

    Make a multiple entry by JavaScript array in webpack.config.js

--- 01_hello/react_app/index.js
+++ 01_hello/react_app/index.js
@@ -1,4 +1,4 @@
-
+console.log('index.js start');
 $( "#root1" ).click(function() {
 	alert("click root1");
 	$(this).text('have been clicked of root1');
@@ -7,3 +7,4 @@ $( "#root2" ).click(function() {
 	alert("click root2");
 	$(this).text('have been clicked of root2');
 });
+console.log('index.js end');
--- 01_hello/webpack.config.js
+++ 01_hello/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 module.exports = {
 	context: path.resolve(__dirname, 'react_app'),
-	entry:  './index.js',
+	entry:  ['./index.js','./main.js' ],
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'bundle.js',
