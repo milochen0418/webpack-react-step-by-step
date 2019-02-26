2cb866c59bbfbc5a29f929ba52a381c5abb6e9e7
Milo Chen <milo_chen@mail2000.com.tw>
2019-02-26 08:19:43 +0000

    update webpack.config.js

--- more_examples/jsx-babel-hello/webpack.config.js
+++ more_examples/jsx-babel-hello/webpack.config.js
@@ -12,15 +12,17 @@ module.exports = {
 
     module: {
 	rules: [
-            { test: /.jsx$/, exclude: /node_modules/, use: { loader: 'babel-loader', options: { presets: ['@babel/preset-react'] } } }
+            { 
+		test: /.jsx$/, 
+		exclude: /node_modules/,
+		use: { 
+			loader: 'babel-loader', 
+			options: { 
+				presets: ['@babel/preset-react'] 
+			} 
+		} 
+	    }
         ],
 
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                loader: 'babel-loader',
-                include: __dirname + '/src',
-            },
-        ],
-    },
+   },
 };
